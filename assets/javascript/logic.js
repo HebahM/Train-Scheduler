b98a9e2d891c003decd4fc73c796d825d4e2f13d
HebahM <56455544+HebahM@users.noreply.github.com>
2020-01-19 17:05:02 -0600

    table refreshes every minute

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -18,7 +18,7 @@ firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 
-$("#submit").on("click", function(event) {
+$("#submit").on("click", function (event) {
     event.preventDefault();
     var trainName = $("#trainName").val();
     // console.log("Train Name: " + trainName)
@@ -40,27 +40,44 @@ $("#submit").on("click", function(event) {
     database.ref("Train Info").push(trainInfo);
 
 })
+function createTable() {
+    $("tbody").empty();
+    database.ref("Train Info").on("child_added", function (childSnapshot) {
+        var trainName = childSnapshot.val().name;
+        var destination = childSnapshot.val().destination;
+        var frequency = childSnapshot.val().frequency;
+        var firstTrain = childSnapshot.val().first;
+        console.log(trainName)
 
-database.ref("Train Info").on("child_added", function(childSnapshot) {
-    var trainName = childSnapshot.val().name;
-    var destination = childSnapshot.val().destination;
-    var frequency = childSnapshot.val().frequency;
-    var firstTrain = childSnapshot.val().first;
-    // console.log(trainName)
+        // calculate next arrival, and time left
+        console.log(firstTrain)
+        var firstTimeConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
+        console.log(firstTimeConverted);
+        var currentTime = moment().format("hh:mm");
+        console.log(currentTime)
+        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        console.log("DIFFERENCE IN TIME: " + diffTime);
+        var tRemainder = diffTime % frequency;
+        console.log(tRemainder);
+        var tMinutesTillTrain = frequency - tRemainder;
+        console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+        var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+        console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
-    // calculate stuff
-    console.log(firstTrain)
-    var firstTimeConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
-    console.log(firstTimeConverted);
+        // create new table row. add train info to row. append row to table
+        var newRow = $("<tr>").append(
+            $("<td>").text(trainName),
+            $("<td>").text(destination),
+            $("<td>").text(frequency),
+            $("<td>").text(moment(nextTrain).format("LT")),
+            $("<td>").text(tMinutesTillTrain)
+        );
+        $("#schedule > tbody").append(newRow);
+    });
 
+}
 
-    // create new table row. add train info to row. append row to table
-    var newRow = $("<tr>").append(
-        $("<td>").text(trainName),
-        $("<td>").text(destination),
-        $("<td>").text(frequency)
-      );
-    $("#schedule > tbody").append(newRow);
-});
+$(document).ready(function(){createTable()});
 
-console.log(moment().format('LT'))
+// refresh every minute
+setInterval(createTable, 60000);
